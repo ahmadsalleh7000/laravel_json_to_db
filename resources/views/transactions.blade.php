389a7f7513d2e31b8382e841cf94f14ee2901d36
ahmadsalleh7000 <ahmad.salleh7000@gmail.com>
2023-09-26 01:04:42 +0300

    change home link in pages header

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -10,7 +10,7 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href="{{URL::to('/')}}">Your App</a>
+            <a class="navbar-brand" href="{{URL::to('/')}}">Home</a>
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link" href="{{URL::to('/transactions')}}">Transactions</a>
